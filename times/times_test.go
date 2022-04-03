abc4dc4e7156ab7009d60fe96ae718d047b83186
retheviper <retheviper@gmail.com>
2022-04-04 01:34:22 +0900

    fix test

--- times/times_test.go
+++ times/times_test.go
@@ -5,29 +5,100 @@ import (
 	"time"
 )
 
+type MonthDay struct {
+	Month int
+	Day   int
+}
+
 func Test_PlusMonths(test *testing.T) {
 
-	date := time.Date(2022, 3, 31, 0, 0, 0, 0, time.UTC)
+	dates := map[*MonthDay]*MonthDay{}
+	dates[&MonthDay{
+		Month: 1,
+		Day:   31,
+	}] = &MonthDay{
+		Month: 2,
+		Day:   28,
+	}
+	dates[&MonthDay{
+		Month: 3,
+		Day:   31,
+	}] = &MonthDay{
+		Month: 4,
+		Day:   30,
+	}
+	dates[&MonthDay{
+		Month: 5,
+		Day:   31,
+	}] = &MonthDay{
+		Month: 6,
+		Day:   30,
+	}
+	dates[&MonthDay{
+		Month: 8,
+		Day:   31,
+	}] = &MonthDay{
+		Month: 9,
+		Day:   30,
+	}
 
-	actual := PlusMonths(date, 1)
+	for fromMonthDay, toMonthDay := range dates {
+		date := time.Date(2022, time.Month(fromMonthDay.Month), fromMonthDay.Day, 0, 0, 0, 0, time.UTC)
+		actual := PlusMonths(date, 1)
+		expected := time.Date(2022, time.Month(toMonthDay.Month), toMonthDay.Day, 0, 0, 0, 0, time.UTC)
 
-	expected := time.Date(2022, 4, 30, 0, 0, 0, 0, time.UTC)
-
-	if actual != expected {
-		test.Errorf("expected %v but was %v", expected, actual)
+		if actual != expected {
+			test.Errorf("expected %v but was %v", expected, actual)
+		}
 	}
 }
 
 func Test_MinusMonths(test *testing.T) {
+	dates := map[*MonthDay]*MonthDay{}
+	dates[&MonthDay{
+		Month: 3,
+		Day:   31,
+	}] = &MonthDay{
+		Month: 2,
+		Day:   28,
+	}
+	dates[&MonthDay{
+		Month: 5,
+		Day:   31,
+	}] = &MonthDay{
+		Month: 4,
+		Day:   30,
+	}
+	dates[&MonthDay{
+		Month: 7,
+		Day:   31,
+	}] = &MonthDay{
+		Month: 6,
+		Day:   30,
+	}
+	dates[&MonthDay{
+		Month: 10,
+		Day:   31,
+	}] = &MonthDay{
+		Month: 9,
+		Day:   30,
+	}
+	dates[&MonthDay{
+		Month: 12,
+		Day:   31,
+	}] = &MonthDay{
+		Month: 11,
+		Day:   30,
+	}
 
-	date := time.Date(2022, 3, 31, 0, 0, 0, 0, time.UTC)
+	for fromMonthDay, toMonthDay := range dates {
+		date := time.Date(2022, time.Month(fromMonthDay.Month), fromMonthDay.Day, 0, 0, 0, 0, time.UTC)
+		actual := MinusMonths(date, 1)
+		expected := time.Date(2022, time.Month(toMonthDay.Month), toMonthDay.Day, 0, 0, 0, 0, time.UTC)
 
-	actual := MinusMonths(date, 1)
-
-	expected := time.Date(2022, 2, 28, 0, 0, 0, 0, time.UTC)
-
-	if actual != expected {
-		test.Errorf("expected %v but was %v", expected, actual)
+		if actual != expected {
+			test.Errorf("expected %v but was %v", expected, actual)
+		}
 	}
 }
 
